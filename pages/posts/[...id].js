7b6e458caa16aca791b9d7334f8bf9942095fdc0
Seongbin Kim <seongbin9786@gmail.com>
2022-09-15 01:11:16 +0900

    tutorial: basics (just before deploy)

---
+++ pages/posts/[...id].js
@@ -0,0 +1,3 @@
+export default function Posts404() {
+  return <h1>Unknown Posts Here!</h1>
+}
---
+++ pages/posts/[id].js
@@ -0,0 +1,42 @@
+import utilStyles from '../../styles/utils.module.css'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import Layout from '../../components/layout';
+import Date from '../../components/date';
+import Head from 'next/head';
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  console.log('getStaticPaths', paths);
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  // Fetch necessary data for the blog post using params.id
+  const postData = await getPostData(params.id);
+  console.log('getStaticProps', postData);
+  return {
+    props: {
+      postData,
+    }
+  }
+}
